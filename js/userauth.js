bdc9a7e8ed153deaf19c230cb3fcf613964ac423
oddzor <odd.grimholt@gokstadakademiet.no>
2024-05-13 21:47:13 +0200

    login/register functionality adjusted

--- js/userauth.js
+++ js/userauth.js
@@ -1,9 +1,12 @@
-const mockBackend = 'https://crudcrud.com/api/5c53611766ca403fb65138445e8faa72';
+const mockBackend = 'https://crudcrud.com/api/49b54a659c37444badaa69070d61b85a';
 
 function registerUser(event) {
     event.preventDefault();
-    const email = document.getElementById('register__email_input').value;
+    const email = document.getElementById('register__email_input').value.toLowerCase();
     const password = document.getElementById('register__password__input').value;
+
+    localStorage.removeItem('userID');
+    localStorage.removeItem('userEmail');
 
     const loginData = {
         email,
@@ -20,7 +23,6 @@ function registerUser(event) {
     .then(response => response.json())
     .then(data => {
         console.log('Registration Successful', data);
-        alert('Registration Successful!');
         toggleForms();
     })
     .catch(error => console.error('Error:', error));
@@ -28,7 +30,7 @@ function registerUser(event) {
 
 function loginUser(event) {
     event.preventDefault();
-    const email = document.getElementById('login__email__input').value;
+    const email = document.getElementById('login__email__input').value.toLowerCase();
     const password = document.getElementById('login__password__input').value;
 
     fetch(mockBackend + '/users')
@@ -37,6 +39,8 @@ function loginUser(event) {
         const user = users.find(u => u.email === email && u.password === password);
         if (user) {
             console.log('Login Successful', user);
+            localStorage.setItem("userID", user._id);
+            localStorage.setItem("userEmail", email);
             alert('Login Successful!');
             document.getElementById("login__form").style.display = "none";
             document.getElementById("toggle__forms").style.display = "none";
@@ -65,4 +69,6 @@ function toggleForms() {
 
 // Sources: 
 
-// Toggle login/register https://stackoverflow.com/questions/71790502/how-to-properly-write-a-javascript-toggle-hide-show-function-for-2-forms-regist
+
+// Register/Login: https://stackoverflow.com/questions/77162708/javascript-role-based-access-failed
+// Inspiration for login/register toggle:  https://stackoverflow.com/questions/71790502/how-to-properly-write-a-javascript-toggle-hide-show-function-for-2-forms-regist
