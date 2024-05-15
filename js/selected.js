0b4756326ff57c268dedf36748a8822b167905b1
oddzor <odd.grimholt@gokstadakademiet.no>
2024-05-15 19:57:56 +0200

    added duplicate email functionality and adding favorites to userid in backend

--- js/selected.js
+++ js/selected.js
@@ -1,52 +1,125 @@
 import { dogFacts } from "./arrays.js";
 
+const mockBackend = "https://crudcrud.com/api/49b54a659c37444badaa69070d61b85a";
 
-function displayDog () { // Dog image population to HTML, retrieved from localStorage
+function displayDog() {
+  // Dog image population to HTML, retrieved from localStorage
   const imageUrl = localStorage.getItem("selectedDogImage");
   const displayedImage = document.getElementById("selected__dog__picture");
   if (displayedImage && imageUrl) {
     displayedImage.src = imageUrl;
   }
-};
-
-function dogName () {
-const dogName = localStorage.getItem("selectedDogName");
-const dogNameElement = document.getElementById("selected__dog__name");
-dogNameElement.textContent = `Name: ${dogName}`;
 }
 
-function randomDogFact() {  // Random dog facts to populate to HTML 
+function dogName() {
+  const dogName = localStorage.getItem("selectedDogName");
+  const dogNameElement = document.getElementById("selected__dog__name");
+  dogNameElement.textContent = `Name: ${dogName}`;
+}
+
+function randomDogFact() {
+  // Random dog facts to populate to HTML
   const randomIndex = Math.floor(Math.random() * dogFacts.length);
   const randomDogFact = dogFacts[randomIndex].fact;
-document.getElementById("random__dog__information").textContent = randomDogFact;
+  document.getElementById("random__dog__information").textContent =
+    randomDogFact;
 }
 
-function fetchBreedName (url) {  // Breed names retrieved from imgUrl, reversed, capitalized and then populate to HTML
-const regex = /breeds\/([^\/]+)\//;
-const match = url.match(regex);
-if (match) {
+function fetchBreedName(url) {
+  // Breed names retrieved from imgUrl, reversed, capitalized and then populate to HTML
+  const regex = /breeds\/([^\/]+)\//;
+  const match = url.match(regex);
+  if (match) {
     const reverseString = match[1].split("-");
-    return reverseString.reverse().map(component => 
-    component.charAt(0).toUpperCase() + component.slice(1)).join(" ");
-}
-return "Breed Not Found";
-}
-
-function displayBreedName () {
-let dogImage = document.getElementById("selected__dog__picture");
-let breed = fetchBreedName(dogImage.src)
-let breedName = document.getElementById("selected__dog__breed");
-breedName.textContent = `Breed: ${breed}`;
+    return reverseString
+      .reverse()
+      .map(
+        (component) => component.charAt(0).toUpperCase() + component.slice(1)
+      )
+      .join(" ");
+  }
+  return "Breed Not Found";
 }
 
-
-window.onload = function () {
-displayDog();
-randomDogFact();
-displayBreedName();
-dogName();
+function displayBreedName() {
+  let dogImage = document.getElementById("selected__dog__picture");
+  let breed = fetchBreedName(dogImage.src);
+  let breedName = document.getElementById("selected__dog__breed");
+  breedName.textContent = `Breed: ${breed}`;
 }
 
+function addToWishlist() {
+  const selectedDogImage = localStorage.getItem("selectedDogImage");
+  const selectedDogName = localStorage.getItem("selectedDogName");
+  const breedElement = document.getElementById("selected__dog__breed");
+  const selectedDogBreed = breedElement
+    ? breedElement.textContent.replace("Breed: ", "")
+    : "Unknown Breed";
+  const userId = localStorage.getItem("userID");
+
+  if (!userId) {
+    console.error("No logged-in user found in localStorage.");
+    alert("Please log in to add to wishlist."); // For use prior to registration/login
+    return;
+  }
+
+  fetch(`${mockBackend}/users/${userId}`)
+    .then((response) => response.json())
+    .then((user) => {
+      const favoriteDog = {
+        // Creating object to store
+        image: selectedDogImage,
+        name: selectedDogName,
+        breed: selectedDogBreed,
+      };
+
+      if (!user.favorites) {
+        user.favorites = []; // Ensuring array exists, redundancy for registration function.
+      }
+
+      user.favorites.push(favoriteDog);
+
+      const simplifiedUser = {
+        email: user.email,
+        password: user.password,
+        favorites: user.favorites,
+      };
+
+      return fetch(`${mockBackend}/users/${userId}`, {
+        method: "PUT", // PUT to "update" information within userdata by adding dogs, chose this method due to possible limitations on subdirectories with crudcrud
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(simplifiedUser),
+      });
+    })
+    .then((response) => {
+      if (!response.ok) {
+        return response.text().then((text) => {
+          console.error("Response text:", text);
+          throw new Error("Network Error");
+        });
+      }
+      if (response.status === 200 || response.status === 204) {
+        return {};
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("Error adding to wishlist:", error);
+    });
+}
+
+window.onload = function () {  // Running necessary function on pageload.
+  displayDog();
+  randomDogFact();
+  displayBreedName();
+  dogName();
+
+  document
+    .getElementById("dog__wishlist__button")
+    .addEventListener("click", addToWishlist);
+};
 
 // Sources:
 
--- js/userauth.js
+++ js/userauth.js
@@ -1,74 +1,88 @@
-const mockBackend = 'https://crudcrud.com/api/49b54a659c37444badaa69070d61b85a';
+const mockBackend = "https://crudcrud.com/api/49b54a659c37444badaa69070d61b85a";
 
 function registerUser(event) {
-    event.preventDefault();
-    const email = document.getElementById('register__email_input').value.toLowerCase();
-    const password = document.getElementById('register__password__input').value;
+  event.preventDefault();
+  const email = document
+    .getElementById("register__email_input")
+    .value.toLowerCase(); // toLowerCase to avoid case sensitive password
+  const password = document.getElementById("register__password__input").value;
 
-    localStorage.removeItem('userID');
-    localStorage.removeItem('userEmail');
+  localStorage.removeItem("userID");
+  localStorage.removeItem("userEmail");
 
-    const loginData = {
+  fetch(mockBackend + "/users")
+    .then((response) => response.json())
+    .then((users) => {
+      const existingUser = users.find((u) => u.email === email);
+      if (existingUser) {
+        alert("Email is already registered."); // Avoiding registration spamming.
+        throw new Error("Email already exists");
+      }
+
+      const loginData = {
         email,
-        password
-    };
+        password,
+        favorites: [], // Initializing empty array to use for favorites/wishlist functionality.
+      };
 
-    fetch(mockBackend + '/users', {
-        method: 'POST',
+      return fetch(mockBackend + "/users", { // Posting loginData to crudcrud backend.
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(loginData)
+        body: JSON.stringify(loginData),
+      });
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Registration Successful', data);
-        toggleForms();
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Registration Successful", data);
+      toggleForms();
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
 
 function loginUser(event) {
-    event.preventDefault();
-    const email = document.getElementById('login__email__input').value.toLowerCase();
-    const password = document.getElementById('login__password__input').value;
+  event.preventDefault();
+  const email = document
+    .getElementById("login__email__input")
+    .value.toLowerCase();
+  const password = document.getElementById("login__password__input").value;
 
-    fetch(mockBackend + '/users')
-    .then(response => response.json())
-    .then(users => {
-        const user = users.find(u => u.email === email && u.password === password);
-        if (user) {
-            console.log('Login Successful', user);
-            localStorage.setItem("userID", user._id);
-            localStorage.setItem("userEmail", email);
-            alert('Login Successful!');
-            document.getElementById("login__form").style.display = "none";
-            document.getElementById("toggle__forms").style.display = "none";
-        } else {
-            console.log('Login Failed: Invalid credentials');
-            alert('Login Failed: Username or Password');
-        }
+  fetch(mockBackend + "/users")
+    .then((response) => response.json())
+    .then((users) => {
+      const user = users.find(
+        (u) => u.email === email && u.password === password
+      );
+      if (user) {
+        console.log("Login Successful", user);
+        localStorage.setItem("userID", user._id); // Adding userID and email to localStorage to simplify usage with selection and favorites.
+        localStorage.setItem("userEmail", email);
+        alert("Login Successful!");
+        document.getElementById("login__form").style.display = "none";
+        document.getElementById("toggle__forms").style.display = "none"; // Hiding login-element when logging in.
+      } else {
+        console.log("Login Failed: Invalid credentials");
+        alert("Login Failed: Username or Password");
+      }
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
 
 function toggleForms() {
-    const loginForm = document.getElementById('login__form');
-    const registrationForm = document.getElementById('registration__form');
+  const loginForm = document.getElementById("login__form");
+  const registrationForm = document.getElementById("registration__form"); // Toggling between register/login forms.
 
-    if (loginForm.style.display === "none") {
-        loginForm.style.display = "block";
-        registrationForm.style.display = "none";
-    } else {
-        loginForm.style.display = "none";
-        registrationForm.style.display = "block";
-    }
+  if (loginForm.style.display === "none") {
+    loginForm.style.display = "block";
+    registrationForm.style.display = "none";
+  } else {
+    loginForm.style.display = "none";
+    registrationForm.style.display = "block";
+  }
 }
 
-
-
-// Sources: 
-
+// Sources:
 
 // Register/Login: https://stackoverflow.com/questions/77162708/javascript-role-based-access-failed
 // Inspiration for login/register toggle:  https://stackoverflow.com/questions/71790502/how-to-properly-write-a-javascript-toggle-hide-show-function-for-2-forms-regist
